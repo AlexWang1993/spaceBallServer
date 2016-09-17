309f6d956ceaca4b4ac9ca09c3302bfd2a452778
Alex <zh7wang@uwaterloo.ca>
2016-09-17 13:39:05 -0400

    first commit

---
+++ src/JettyServer.java
@@ -0,0 +1,34 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+public class JettyServer {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        WebSocketHandler wsHandler = new WebSocketHandler() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.register(GameWebSocketHandler.class);
+            }
+        };
+        ServletContextHandler ctx = new ServletContextHandler();
+        ctx.setContextPath("/");
+        ctx.addServlet(GameServiceSocketServlet.class, "/socket");
+ 
+        server.setHandler(ctx);
+ 
+        server.start();
+        server.join();
+    }
+    
+    public static class GameServiceSocketServlet extends WebSocketServlet {
+    	 
+        @Override
+        public void configure(WebSocketServletFactory factory) {
+            factory.register(GameWebSocketHandler.class);
+        }
+    }
+}
